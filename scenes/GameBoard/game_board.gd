191edfa72bc4b4797ae7812c063bcbbce788a499
GabrielLeanca <gabriel.leanca@gmail.com>
2026-08-26 19:06:45 +0300

    start work on routes

--- scenes/GameBoard/game_board.gd
+++ scenes/GameBoard/game_board.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
 var camera2d
+var map
+var locations
 var routes = [
 	{"start":1, "end":2, "obstacles":["darkness"], "isOneWay":true},
-	{},
-	{}
+	{"start":1, "end":3, "obstacles":["darkness"], "isOneWay":false},
+	{"start":1, "end":4, "obstacles":["darkness"], "isOneWay":true}
 ]
 
 func _on_ready() -> void:
 	camera2d = get_node("Camera2D")
+	map = get_node("Map")
+	locations = map.get_children()
+	
+	for route in routes:
+		pass
