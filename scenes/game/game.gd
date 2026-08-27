166567a2e6ec4e1f5153a7b736e92d6db5228695
Has Constantin-Marian <hasconstantin@proton.me>
2026-08-28 00:59:05 +0300

    Simplify pause logic and remove MainGame dupe scene

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,1 +1,8 @@
-extends Node2D
+extends Node
+
+@onready var pause_menu: Control = $PauseLayer/PauseMenu
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().paused = !get_tree().paused
+		pause_menu.visible = !pause_menu.visible
