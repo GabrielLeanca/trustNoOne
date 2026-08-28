1813946a4b3abd96070f7e4542b3a94af1a629fd
GabrielLeanca <gabriel.leanca@gmail.com>
2026-08-28 22:04:08 +0300

    change variable names

--- globals.gd
+++ globals.gd
@@ -3,15 +3,15 @@ extends Node
 var characterList = ["Oscar"]
 var initialCharactersAvailable = ["Oscar"]
 var actionList = []
-var initialActionsAvailable = []
+var initialCardsAvailable = []
 
 
 var charactersAvailable = []
-var actionsAvailable = []
+var cardsAvailable = []
 var isFirstGame = true #if we want more runs, make these be read from file
 
 func _init() -> void:
 	#read value of isFirstGame here
 	if isFirstGame:
 		charactersAvailable = initialCharactersAvailable
-		actionsAvailable = initialCharactersAvailable
+		cardsAvailable = initialCharactersAvailable
