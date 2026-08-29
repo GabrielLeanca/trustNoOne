5f7d3bbe7a8358780a09c8c83020a4c1b9271a72
GabrielLeanca <gabriel.leanca@gmail.com>
2026-08-29 20:28:24 +0300

    add role to character menu

--- scenes/UI/HUD/CharacterCard/character_card.gd
+++ scenes/UI/HUD/CharacterCard/character_card.gd
@@ -4,8 +4,10 @@ var character: Explorer = null
 @onready var description: Label = $Description
 @onready var avatar: TextureRect = $Avatar
 @onready var cname: Label = $Name
+@onready var role: Label = $Role
 
 func view():
 	avatar.texture = character.portrait
 	cname.text = character.name
 	description.text = character.description
+	role.text = character.role
