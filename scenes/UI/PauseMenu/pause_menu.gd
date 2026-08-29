47781f598a96423993a498f5a77ab83b0f651359
Has Constantin-Marian <hasconstantin@proton.me>
2026-08-29 14:00:11 +0300

    Add pause menu button and credits

--- scenes/UI/PauseMenu/pause_menu.gd
+++ scenes/UI/PauseMenu/pause_menu.gd
@@ -1,7 +1,10 @@
 extends Control
 
+@onready var credits_text: Label = $CenterContainer/VBoxContainer/CreditsText
+
 func _ready() -> void:
 	self.visible = false
+	credits_text.visible = false
 
 func _on_resume_pressed() -> void:
 	get_tree().paused = false
@@ -10,3 +13,6 @@ func _on_resume_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_credits_pressed() -> void:
+	credits_text.visible = !credits_text.visible
